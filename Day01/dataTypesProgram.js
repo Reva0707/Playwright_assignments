d1f8ec1753d7c0bbe7900e46a1695ac193b2f964
Reva0707 <revatijreddi77@gmail.com>
2026-07-21 09:18:48 +0530

    Add files via upload
    day01 assignment

---
+++ Day01/dataTypesProgram.js
@@ -0,0 +1,11 @@
+let firstName = "Revati"
+let companyName = "virtusa"
+let mobileNumber = 1234567890
+let isAutomation = true
+let hasPlaywright
+
+console.log("firstName"+" is "+typeof firstName)
+console.log("companyName"+" is "+typeof companyName)
+console.log("mobileNumber"+" is "+typeof mobileNumber)
+console.log("isAutomation"+" is "+typeof isAutomation)
+console.log("hasPlaywright"+" is "+typeof hasPlaywright)
